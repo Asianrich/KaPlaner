b226ac73b81a4ba2475b881580f18a1424d5de2f
jpinnecker <joshua-p@gmx.de>
2019-01-08 13:09:46 +0100

    Applied structural changes

--- KaPlanerClient/Logic/Interface.cs
+++ KaPlanerClient/Logic/Interface.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace KaPlaner.KaPlaner.KaPlaner.Logic
+namespace KaPlaner.KaPlanerClient.Logic
 {
     interface Interface
     {
