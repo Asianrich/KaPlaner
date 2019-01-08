f1a65484561d2b762bc82a0a7c37ee198514cf99
jpinnecker <jpinnecker@htwsaar.de>
2019-01-08 13:03:09 +0100

    New folder structure

---
+++ KaPlanerClient/Database/IDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaPlaner.Database
+{
+    interface IDatabase
+    {
+
+        void registerUser(string username, string password);
+
+
+
+
+    }
+}
