4a278af8f6d09af6c18b1c6235c6c0c6820af0a2
Swathi_Su <Swathi_Su@DESKTOP-HPRS7KQ>
2019-03-11 09:42:20 +0100

    IDatabse.Server

---
+++ KaPlanerServer/Database/IDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaPlaner.Database
+{
+    interface IDatabase
+    {
+
+        bool registerUser(string username, string password, string password_bestaetigen);
+
+        bool login(string username, string password);
+
+
+
+
+    }
+}
