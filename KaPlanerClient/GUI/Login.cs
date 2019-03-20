07bfac92a8f8fa3151cf0e41e286a7cf2b39b780
jpinnecker <joshua-p@gmx.de>
2019-03-21 00:50:32 +0100

    Nachtrag zu static clientlogic

--- KaPlanerClient/GUI/Login.cs
+++ KaPlanerClient/GUI/Login.cs
@@ -165,7 +165,7 @@ namespace WindowsFormsApp1
             {
                 if (clientLogic.LoginRemote(new User(tb_log_benutzername.Text, tb_log_passwort.Text)))
                 {
-                    Form open_calendar = new wdw_calendar(clientLogic, true);
+                    Form open_calendar = new wdw_calendar(true);
                     open_calendar.Show();
                     tb_log_benutzername.Text = "";
                     tb_log_passwort.Text = "";
@@ -200,7 +200,7 @@ namespace WindowsFormsApp1
             {
                 if (clientLogic.LoginLocal(new User(tb_log_benutzername.Text, tb_log_passwort.Text)))
                 {
-                    Form open_calendar = new wdw_calendar(clientLogic, false);
+                    Form open_calendar = new wdw_calendar(false);
                     open_calendar.Show();
                     tb_log_benutzername.Text = "";
                     tb_log_passwort.Text = "";
