716ddff0e548781eb65e82629f6dc71a6b62c157
Swathi_Su <Swathi_Su@DESKTOP-HPRS7KQ>
2019-07-07 18:51:46 +0200

    Hierarchy

---
+++ KaObjects/HierarchyPackage.cs
@@ -0,0 +1,37 @@
+﻿//using Microsoft.Analytics.Interfaces;
+//using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KaObjects
+{
+
+    public enum Hierarchyrequest { NewServer, Register, Login, Invite }
+    public class HierarchyPackage
+    {
+        public int parent;          //root-server
+        public int child;           //Kinder-Knoten
+        public int number;          // jedes Kind hat ne Kennung
+        public int level;           // Ebene-Erweiterung
+
+
+        public Hierarchyrequest hierarchyrequest; 
+        // // 
+
+
+        //Funktionen//
+
+
+
+
+
+
+
+
+
+
+    }
+}
