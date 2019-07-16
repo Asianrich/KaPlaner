6c621752fa40a72d2e695c397b02be8e47c8ecf2
Malak <Malak@DESKTOP-CCLSKH2>
2019-07-16 10:57:15 +0200

    Vorschaufenster Teil III

--- KaPlanerClient/GUI/Vorschaufenster.Designer.cs
+++ KaPlanerClient/GUI/Vorschaufenster.Designer.cs
@@ -28,70 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LV_dates = new System.Windows.Forms.ListView();
-            this.Titel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Ort = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Beginn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Ende = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbl_beschreibung = new System.Windows.Forms.Label();
+            this.lbl_beginn = new System.Windows.Forms.Label();
+            this.lbl_ort = new System.Windows.Forms.Label();
+            this.lbl_titel = new System.Windows.Forms.Label();
+            this.TB_Place = new System.Windows.Forms.TextBox();
+            this.TB_Title = new System.Windows.Forms.TextBox();
+            this.lbl_end = new System.Windows.Forms.Label();
+            this.tb_Ende = new System.Windows.Forms.TextBox();
+            this.tB_Beginn = new System.Windows.Forms.TextBox();
+            this.rTB_Beschreibung = new System.Windows.Forms.RichTextBox();
+            this.tB_TerminID = new System.Windows.Forms.TextBox();
+            this.lbl_ID = new System.Windows.Forms.Label();
+            this.tb_Verantwortlicher = new System.Windows.Forms.TextBox();
+            this.lbl_Verantwortlicher = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // LV_dates
+            // lbl_beschreibung
             // 
-            this.LV_dates.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Titel,
-            this.Ort,
-            this.Beginn,
-            this.Ende});
-            this.LV_dates.Font = new System.Drawing.Font("Arial", 12F);
-            this.LV_dates.FullRowSelect = true;
-            this.LV_dates.GridLines = true;
-            this.LV_dates.HideSelection = false;
-            this.LV_dates.Location = new System.Drawing.Point(12, 10);
-            this.LV_dates.Name = "LV_dates";
-            this.LV_dates.Size = new System.Drawing.Size(657, 401);
-            this.LV_dates.TabIndex = 1;
-            this.LV_dates.UseCompatibleStateImageBehavior = false;
-            this.LV_dates.View = System.Windows.Forms.View.Details;
-            this.LV_dates.SelectedIndexChanged += new System.EventHandler(this.LV_dates_SelectedIndexChanged);
+            this.lbl_beschreibung.AutoSize = true;
+            this.lbl_beschreibung.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_beschreibung.Location = new System.Drawing.Point(55, 246);
+            this.lbl_beschreibung.Name = "lbl_beschreibung";
+            this.lbl_beschreibung.Size = new System.Drawing.Size(151, 24);
+            this.lbl_beschreibung.TabIndex = 36;
+            this.lbl_beschreibung.Text = "Beschreibung";
             // 
-            // Titel
+            // lbl_beginn
             // 
-            this.Titel.Text = "Titel";
-            this.Titel.Width = 250;
+            this.lbl_beginn.AutoSize = true;
+            this.lbl_beginn.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_beginn.Location = new System.Drawing.Point(125, 172);
+            this.lbl_beginn.Name = "lbl_beginn";
+            this.lbl_beginn.Size = new System.Drawing.Size(81, 24);
+            this.lbl_beginn.TabIndex = 35;
+            this.lbl_beginn.Text = "Beginn";
             // 
-            // Ort
+            // lbl_ort
             // 
-            this.Ort.Text = "Ort";
-            this.Ort.Width = 200;
+            this.lbl_ort.AutoSize = true;
+            this.lbl_ort.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ort.Location = new System.Drawing.Point(165, 135);
+            this.lbl_ort.Name = "lbl_ort";
+            this.lbl_ort.Size = new System.Drawing.Size(41, 24);
+            this.lbl_ort.TabIndex = 34;
+            this.lbl_ort.Text = "Ort";
             // 
-            // Beginn
+            // lbl_titel
             // 
-            this.Beginn.Text = "Beginn";
-            this.Beginn.Width = 100;
+            this.lbl_titel.AutoSize = true;
+            this.lbl_titel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titel.Location = new System.Drawing.Point(154, 97);
+            this.lbl_titel.Name = "lbl_titel";
+            this.lbl_titel.Size = new System.Drawing.Size(52, 24);
+            this.lbl_titel.TabIndex = 33;
+            this.lbl_titel.Text = "Titel";
             // 
-            // Ende
+            // TB_Place
             // 
-            this.Ende.Text = "Ende";
-            this.Ende.Width = 100;
+            this.TB_Place.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TB_Place.Location = new System.Drawing.Point(212, 132);
+            this.TB_Place.Name = "TB_Place";
+            this.TB_Place.ReadOnly = true;
+            this.TB_Place.Size = new System.Drawing.Size(442, 32);
+            this.TB_Place.TabIndex = 32;
+            // 
+            // TB_Title
+            // 
+            this.TB_Title.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TB_Title.Location = new System.Drawing.Point(212, 94);
+            this.TB_Title.Name = "TB_Title";
+            this.TB_Title.ReadOnly = true;
+            this.TB_Title.Size = new System.Drawing.Size(442, 32);
+            this.TB_Title.TabIndex = 31;
+            // 
+            // lbl_end
+            // 
+            this.lbl_end.AutoSize = true;
+            this.lbl_end.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_end.Location = new System.Drawing.Point(144, 211);
+            this.lbl_end.Name = "lbl_end";
+            this.lbl_end.Size = new System.Drawing.Size(62, 24);
+            this.lbl_end.TabIndex = 38;
+            this.lbl_end.Text = "Ende";
+            // 
+            // tb_Ende
+            // 
+            this.tb_Ende.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Ende.Location = new System.Drawing.Point(212, 208);
+            this.tb_Ende.Name = "tb_Ende";
+            this.tb_Ende.ReadOnly = true;
+            this.tb_Ende.Size = new System.Drawing.Size(442, 32);
+            this.tb_Ende.TabIndex = 39;
+            // 
+            // tB_Beginn
+            // 
+            this.tB_Beginn.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tB_Beginn.Location = new System.Drawing.Point(212, 169);
+            this.tB_Beginn.Name = "tB_Beginn";
+            this.tB_Beginn.ReadOnly = true;
+            this.tB_Beginn.Size = new System.Drawing.Size(442, 32);
+            this.tB_Beginn.TabIndex = 40;
+            // 
+            // rTB_Beschreibung
+            // 
+            this.rTB_Beschreibung.Location = new System.Drawing.Point(212, 246);
+            this.rTB_Beschreibung.Name = "rTB_Beschreibung";
+            this.rTB_Beschreibung.ReadOnly = true;
+            this.rTB_Beschreibung.Size = new System.Drawing.Size(442, 243);
+            this.rTB_Beschreibung.TabIndex = 41;
+            this.rTB_Beschreibung.Text = "";
+            // 
+            // tB_TerminID
+            // 
+            this.tB_TerminID.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tB_TerminID.Location = new System.Drawing.Point(212, 21);
+            this.tB_TerminID.Name = "tB_TerminID";
+            this.tB_TerminID.ReadOnly = true;
+            this.tB_TerminID.Size = new System.Drawing.Size(442, 32);
+            this.tB_TerminID.TabIndex = 42;
+            // 
+            // lbl_ID
+            // 
+            this.lbl_ID.AutoSize = true;
+            this.lbl_ID.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ID.Location = new System.Drawing.Point(108, 24);
+            this.lbl_ID.Name = "lbl_ID";
+            this.lbl_ID.Size = new System.Drawing.Size(98, 24);
+            this.lbl_ID.TabIndex = 43;
+            this.lbl_ID.Text = "TerminID";
+            // 
+            // tb_Verantwortlicher
+            // 
+            this.tb_Verantwortlicher.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Verantwortlicher.Location = new System.Drawing.Point(212, 59);
+            this.tb_Verantwortlicher.Name = "tb_Verantwortlicher";
+            this.tb_Verantwortlicher.ReadOnly = true;
+            this.tb_Verantwortlicher.Size = new System.Drawing.Size(442, 32);
+            this.tb_Verantwortlicher.TabIndex = 44;
+            // 
+            // lbl_Verantwortlicher
+            // 
+            this.lbl_Verantwortlicher.AutoSize = true;
+            this.lbl_Verantwortlicher.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Verantwortlicher.Location = new System.Drawing.Point(34, 62);
+            this.lbl_Verantwortlicher.Name = "lbl_Verantwortlicher";
+            this.lbl_Verantwortlicher.Size = new System.Drawing.Size(172, 24);
+            this.lbl_Verantwortlicher.TabIndex = 45;
+            this.lbl_Verantwortlicher.Text = "Verantwortlicher";
             // 
             // Vorschaufenster
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 446);
-            this.Controls.Add(this.LV_dates);
+            this.ClientSize = new System.Drawing.Size(666, 503);
+            this.Controls.Add(this.lbl_Verantwortlicher);
+            this.Controls.Add(this.tb_Verantwortlicher);
+            this.Controls.Add(this.lbl_ID);
+            this.Controls.Add(this.tB_TerminID);
+            this.Controls.Add(this.rTB_Beschreibung);
+            this.Controls.Add(this.tB_Beginn);
+            this.Controls.Add(this.tb_Ende);
+            this.Controls.Add(this.lbl_end);
+            this.Controls.Add(this.lbl_beschreibung);
+            this.Controls.Add(this.lbl_beginn);
+            this.Controls.Add(this.lbl_ort);
+            this.Controls.Add(this.lbl_titel);
+            this.Controls.Add(this.TB_Place);
+            this.Controls.Add(this.TB_Title);
             this.Name = "Vorschaufenster";
             this.Text = "Terminvorschau";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView LV_dates;
-        private System.Windows.Forms.ColumnHeader Titel;
-        private System.Windows.Forms.ColumnHeader Ort;
-        private System.Windows.Forms.ColumnHeader Beginn;
-        private System.Windows.Forms.ColumnHeader Ende;
+        private System.Windows.Forms.Label lbl_beschreibung;
+        private System.Windows.Forms.Label lbl_beginn;
+        private System.Windows.Forms.Label lbl_ort;
+        private System.Windows.Forms.Label lbl_titel;
+        private System.Windows.Forms.TextBox TB_Place;
+        private System.Windows.Forms.TextBox TB_Title;
+        private System.Windows.Forms.Label lbl_end;
+        private System.Windows.Forms.TextBox tb_Ende;
+        private System.Windows.Forms.TextBox tB_Beginn;
+        private System.Windows.Forms.RichTextBox rTB_Beschreibung;
+        private System.Windows.Forms.TextBox tB_TerminID;
+        private System.Windows.Forms.Label lbl_ID;
+        private System.Windows.Forms.TextBox tb_Verantwortlicher;
+        private System.Windows.Forms.Label lbl_Verantwortlicher;
     }
 }
