7765f0abcabd2dd2b36e1a1c235c23d078804eab
Swathi_Su <Swathi_Su@DESKTOP-HPRS7KQ>
2019-07-16 13:17:36 +0200

    UnTe

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -441,5 +441,14 @@ namespace UnitTest
             //Console.WriteLine(check.ToString());
 
         }
+
+        public void SaveEventsPruefung()
+        {
+            int test = 6;
+            Database testdb = new Database("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Data\\User_Calendar.mdf;Integrated Security = True");
+            List<Package> testlist = new List<Package>();
+
+            //testdb.SaveInvites(testlist, testtermin);
+        }
     }
 }
